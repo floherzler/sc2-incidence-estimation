185de29b482caf2637199c7c49a72b4e6221e565
Nora Wild <noraw61@zedat.fu-berlin.de>
2024-10-01 17:19:08 +0200

    font size, figsize...

--- scripts/histogram.py
+++ scripts/histogram.py
@@ -4,26 +4,8 @@ import matplotlib.dates as mdates
 from matplotlib.patches import Patch
 
 
-#histogram for sequences per day
-#df = pd.read_csv('../data/sonar_out_fr.csv', parse_dates=['date'], dayfirst=False) 
-#date_counts = df['date'].value_counts().sort_index()  # Count occurrences per date and sort by date
-#plt.figure(figsize=(10, 6))
-#weekdays = pd.to_datetime(date_counts.index).day_name()  # Get the day of the week for each date
-#colors = ['red' if day == 'Monday' else 'blue' for day in weekdays]  #
-#plt.bar(date_counts.index, date_counts.values, color=colors, width=0.8, edgecolor='black')
-#from matplotlib.patches import Patch
-#legend_elements = [Patch(facecolor='red', edgecolor='black', label='Monday'),
-#                   Patch(facecolor='blue', edgecolor='black', label='Other Days')]
-#plt.legend(handles=legend_elements)
-#plt.xlabel('Date')
-#plt.ylabel('Number of Entries')
-#plt.title('Number of sequences per Day')
-#plt.xticks(rotation=45)
-#plt.tight_layout() 
-#plt.savefig('../results/plots/hist_seq.png', dpi=300, bbox_inches='tight') 
+plt.rcParams.update({'font.size': 14})
 
-#------------------------------------------------------------------------------------------------
-# Define function to plot histogram
 def plot_histogram(df, ax, title):
     date_counts = df['date'].value_counts().sort_index()  # Count occurrences per date and sort by date
     weekdays = pd.to_datetime(date_counts.index).day_name()  # Get the day of the week for each date
@@ -39,7 +21,7 @@ df1 = pd.read_csv('data/sonar_out_fr.csv', parse_dates=['date'], dayfirst=False)
 df2 = pd.read_csv('data/sonar_out_sp.csv', parse_dates=['date'], dayfirst=False)
 df3 = pd.read_csv('data/given/germany/sequences_Germany_220101_220630.csv', parse_dates=['date'], dayfirst=False)
 
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(14, 18), sharex=True)
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(20, 12), sharex=True)
 
 plot_histogram(df1, axes[0], 'Number of sequences per Day (France)')
 plot_histogram(df2, axes[1], 'Number of sequences per Day (Spain)')
